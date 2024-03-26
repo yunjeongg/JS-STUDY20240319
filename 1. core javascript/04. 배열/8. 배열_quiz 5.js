aec4c68370506f7fb016a3fb877fb64e3d23b03e
yunjeongg <bae.yunjeongg@gmail.com>
2024-03-26 09:06:23 +0900

    # 배열 퀴즈 답안 추가

--- 1. core javascript/04. 배열/8. 배열_quiz 5.js
+++ 1. core javascript/04. 배열/8. 배열_quiz 5.js
@@ -13,6 +13,40 @@
 4-2. 삭제할 새로운 멤버의 이름을 입력하고 한 명의 멤버를 정확하게 수정할때까지 프로그램은 계속되어야 합니다.
 5-2. 프로그램을 종료하세요.
 */
+
+
+/* 선생님 정답
+
+var tvxq = ['유노윤호', '최강창민', '영웅재중', '믹키유천', '시아준수'];
+
+while (true) {
+  var choice = prompt(
+    `현재 멤버: [${tvxq}]\n메뉴를 선택하세요.\n1. 새로운 이름 추가\n2. 기존 이름 삭제\n3. 프로그램 종료`
+  );
+  if (choice === '1') {
+    var newName = prompt('추가할 새로운 멤버의 이름을 입력하세요.');
+    tvxq.push(newName);
+    alert(`${newName}이(가) 추가되었습니다.`);
+  } else if (choice === '2') {
+    var delName = prompt('삭제할 멤버의 이름을 입력하세요.');
+    var idx = tvxq.indexOf(delName);
+    if (idx !== -1) {
+      tvxq.splice(idx, 1);
+      alert(`${delName}이(가) 삭제되었습니다.`);
+    } else {
+      alert(`${delName}은(는) 잘못된 이름입니다.\n다시 입력하세요!`);
+    }
+  } else if (choice === '3') {
+    alert('프로그램을 종료합니다.');
+    break;
+  } else {
+    alert('잘못된 입력입니다. 메뉴 번호를 정확하게 입력해주세요.');
+  }
+}
+
+*/
+
+
 var tvxq = ["유노윤호", "최강창민", "영웅재중", "믹키유천", "시아준수"];
 
 while (true) {
