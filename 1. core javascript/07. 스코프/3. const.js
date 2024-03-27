a81ea31a233795f994071122c7fab06959d89d51
yunjeongg <bae.yunjeongg@gmail.com>
2024-03-27 17:37:33 +0900

    # 스코프

---
+++ 1. core javascript/07. 스코프/3. const.js
@@ -0,0 +1,41 @@
+
+let x;
+x = 'zz';
+x = 'ff';
+console.log(x);
+
+// const : 상수 선언
+const y = '메롱';
+// y = 'ggg';
+console.log(y);
+
+const PI = 3.14159265;
+const COLOR_GREEN = '#0f0';
+/*
+  $div.style.background = COLOR_GREEN;
+*/
+
+// 상수는 객체의 불변성을 유지할 수 있다.
+const kim = {
+  name : '김철수',
+  age : 30
+};
+kim.age = 31;
+kim.name = '마이클 킴';
+
+// kim = {
+//   name : '김철수',
+//   age : 30
+// };
+
+const array = {
+  0: 'z',
+  1: 'dd',
+  2: 'ddf',
+  length: 3
+};
+
+
+const userName = 'zz';
+
+for (let i = 0; i < 5; i++) {}
